86915e28615fa7af99f526e04fe8e32e0ad50eed
Mustapha Abourar <101810866+Devmustroc@users.noreply.github.com>
2023-02-20 20:51:33 +0100

    Create a file with the name and text entered by the user inside a Container

--- nodeJs-Container-python/node/express/files/index.js
+++ nodeJs-Container-python/node/express/files/index.js
@@ -1,15 +1,22 @@
-const fs = require('fs')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+/**
+ * @file index.js
+ * @description NodeJS File System
+ * Create a file with the name and text entered by the user inside a Container
+ */
+
+
+const fs = require('fs')  // File System
+const readline = require('readline').createInterface({  // Readline
+    input: process.stdin,  // Input
+    output: process.stdout  // Output
 })
 
-readline.question('Enter filename: ', filename => {
-    readline.question('Enter some text: ', text => {
-        fs.writeFile(`${filename}.txt`, text, err => {
-            if (err) throw err
-            console.log('File was created')
-            readline.close()
+readline.question('Enter filename: ', filename => { // Question
+    readline.question('Enter some text: ', text => { // Question
+        fs.writeFile(`${filename}.txt`, text, err => { // Write file
+            if (err) throw err // Error
+            console.log('File was created') // Success
+            readline.close() // Close
         })
     })
 })
